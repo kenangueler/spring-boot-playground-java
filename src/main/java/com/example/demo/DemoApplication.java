956d6877f4f748807d3b15c17814a33b00a3cb12
Kenan Güler <15689800+kenangueler@users.noreply.github.com>
2025-06-24 12:49:20 +0000

    Add boilerplate code to execute on application ready event

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void init() {
+		log.info("Application is ready to use!");
+	}
 }
